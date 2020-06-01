b65111249add9cca6de5ce368896860f90695ad5
fatimasajadi <s.sfatemeh@gmail.com>
2020-06-01 15:29:33 +0000

    cleam up

--- spinner.js
+++ spinner.js
@@ -11,28 +11,27 @@ setTimeout(() => {
 }, 500);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
+
   process.stdout.write('\r\\   ');
 }, 700);
 
-// ... fill in the rest yourself ...
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
+
   process.stdout.write('\r|   ');
 }, 900);
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
+
   process.stdout.write('\r/   ');
 }, 1100);
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
+
   process.stdout.write('\r-  ');
 }, 1300);
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
+
   process.stdout.write('\r\\   ');
 }, 1500);
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
+
   process.stdout.write('\r|   ');
 }, 1700);
